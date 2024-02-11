c67b99608f90095cea70dbc54a5d017f69647858
Rob Turner <robturner2656@gmail.com>
2024-02-11 01:15:28 -0600

    removing chakra ui from footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,4 @@
 import React from "react";
-import {Box, Flex} from "@chakra-ui/react";
-
-// const Footer = () => {
-//   return (
-//     <Box backgroundColor="#18181b">
-//       <footer>
-//         <Flex
-//           margin="0 auto"
-//           px={12}
-//           color="white"
-//           justifyContent="center"
-//           alignItems="center"
-//           maxWidth="1024px"
-//           height={16}
-//         >
-//           <p>RobT • © 2024</p>
-//         </Flex>
-//       </footer>
-//     </Box>
-//   );
 
 const Footer = () => {
   return (
